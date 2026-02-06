f61da609464a35ecfc8c0e6b83844725ff261b08
marcus <marcusoletuno@gmail.com>
2026-02-06 11:10:38 +0300

    added updates

---
+++ lesson1b.py
@@ -0,0 +1,23 @@
+# introduction to python
+print("hello world")
+
+# addition
+
+print(2+3)
+
+# subtraction
+print(3-1)
+
+# multiplacation
+
+print(2*3)
+
+# divison
+
+print(21/3)
+
+# exponential
+print(3**10)
+
+# modulus
+print(3%6)
